4d3085051a1bbdfd0c615ca0fddb5dc93324905c
ZG82 <2700204713@qq.com>
2025-12-29 23:41:41 +0800

    “完善”了一下设置页

--- Page_setting.cpp
+++ Page_setting.cpp
@@ -7,7 +7,10 @@ void page_setting::drawPage() {
 	settextstyle(30, 0, L"微软雅黑");
 	outtextxy(230, 540, L"游戏设置页");  //调整窗口大小之后文字需要重新填写坐标
 
-	
+	cleardevice();
+	settextcolor(RGB(0, 0, 255));
+	settextstyle(80, 0, L"微软雅黑");
+	outtextxy(150, 250, L"正在开发中...");
 
 	button btnBack(450, 0, 150, 60, L"返回");
 
